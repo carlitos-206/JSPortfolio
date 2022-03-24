7edd83e79ad779596e313c9826ce265e94cab433
carlitos-206 <carloscaceres041@gmail,com>
2022-03-23 18:46:40 -0700

    Front-end 90% complete

--- public/uwReqs.js
+++ public/uwReqs.js
@@ -1,3 +1,4 @@
+
 const arrivalProfilePic = document.getElementById('arrivalProfilePic')
 const arrow = document.getElementsByClassName('arrow')[0]
 const arrivalDIV = document.getElementsByClassName('arrivalDIV')[0]
@@ -6,9 +7,16 @@ const carusel_btn = document.getElementById('btn-text')
 const fish = document.getElementById('fishGIF')
 const lofiMusic = document.getElementById("lofiMusic")
 const iframe = document.querySelector('iframe')
+const gitLogo = document.getElementById('gitLogo')
+const projectGitLogo = document.getElementById('gitLogo2')
 // This function checks the screen dimensions
 const screenSettting = ()=>{
     // if the screen is the less than 1024px Don't allow access
+    if(screen.width < 1024){
+        alert(`
+        Please view this on a Desktop
+        Mobile screens will be supported in the future`)
+    }
     if(screen.width<1920){
         alert(`
         Your Screen is ${screen.width}px wide and ${screen.height}px height.
@@ -22,6 +30,12 @@ const screenSettting = ()=>{
         arrow.setAttribute('style', 'display: none')
         arrivalDIV.setAttribute('style', 'display:none')
         baseProfile.setAttribute('style', 'display:block')
+        setTimeout(()=>{
+            gitLogo.setAttribute('class', 'animate__animated animate__bounce')
+        }, 2000)
+        setTimeout(()=>{
+            projectGitLogo.setAttribute('class', 'animate__animated animate__bounce')
+        }, 4000)
     }else{
         
         // the screen is a desktop size, loads .baseProfile and removes .arrivalDiv
@@ -30,12 +44,18 @@ const screenSettting = ()=>{
         arrow.setAttribute('style', 'display: none')
         arrivalDIV.setAttribute('style', 'display:none')
         baseProfile.setAttribute('style', 'display:block')
+        setTimeout(()=>{
+            gitLogo.setAttribute('class', 'animate__animated animate__bounce')
+        }, 2000)
+        setTimeout(()=>{
+            projectGitLogo.setAttribute('class', 'animate__animated animate__bounce')
+        }, 4000)
     }
 }
 // if the user hasn't clicked the img then display an arrow
 setTimeout(() => {
     arrow.setAttribute('style', 'display: block')
-}, 5000);
+}, 2000);
 
 // Click the img to load the next phase
 arrivalProfilePic.addEventListener('click', ()=>{
@@ -94,37 +114,54 @@ const mainPromise = new Promise((resolve, reject)=>{
             }
         }, "json")
     })
-
 mainPromise
     .then((message)=>{
+        hellTest = message
         let city = message.city
         let url = `https://weatherdbi.herokuapp.com/data/weather/${city}`
+        let thisDay = document.getElementById('Day');
+        let thisIMG = document.getElementById('weatherIMG')
+        let thisTemp = document.getElementById('temp')
+        let thisComment = document.getElementById('mainComment')
+        let tomorrow = document.getElementById('2moro');
+        let tomorrowIMG = document.getElementById('2moroWeatherIMG')
+        let tomorrowTemp = document.getElementById('2moroTemp')
+        let tomorrowComment = document.getElementById('2moroMainComment')
+        let locationWeather = document.getElementById('locationWeather')
         console.log(url)
         fetch(url)
             .then(res=>res.json())
-            .then(json=>console.log('hrer',json.next_days))
-            .catch((response) => {
-            console.log(response.next_days)
-                })
-    }).catch((message)=>{
+            .then(json=>{
+                thisDay.textContent = `Today: ${json.next_days[0].day}`
+                thisIMG.setAttribute('src', json.next_days[0].iconURL)
+                thisTemp.textContent = `${json.next_days[0].max_temp.f}F / ${json.next_days[0].max_temp.c}C `
+                thisComment.textContent = json.next_days[0].comment
+                tomorrow.textContent = `Tomorrow: ${json.next_days[1].day}`
+                tomorrowIMG.setAttribute('src', json.next_days[1].iconURL)
+                tomorrowTemp.textContent = `${json.next_days[1].max_temp.f}F / ${json.next_days[0].max_temp.c}C `
+                tomorrowComment.textContent = json.next_days[1].comment
+                locationWeather.textContent = `location: ${message.city}`
+            })
+        })
+    .catch((message)=>{
             let city = message
             let url = `https://weatherdbi.herokuapp.com/data/weather/${city}`
             console.log(url)
             fetch(url)
             .then(res=>res.json())
-            .then(json=>console.log('hrer',json.next_days))
-            .catch((response) => {
-                console.log(response.next_days)
+            .then(json=>{
+                thisDay.textContent = `Today: ${json.next_days[0].day}`
+                thisIMG.setAttribute('src', json.next_days[0].iconURL)
+                thisTemp.textContent = `${json.next_days[0].max_temp.f}F / ${json.next_days[0].max_temp.c}C `
+                thisComment.textContent = json.next_days[0].comment
+                tomorrow.textContent = `Tomorrow: ${json.next_days[1].day}`
+                tomorrowIMG.setAttribute('src', json.next_days[1].iconURL)
+                tomorrowTemp.textContent = `${json.next_days[1].max_temp.f}F / ${json.next_days[0].max_temp.c}C `
+                tomorrowComment.textContent = json.next_days[1].comment
+                locationWeather.textContent = `Default City: ${message.city}`
             })
 })
 
-// console.log('ipsolution', test)
-// console.log('ipsolution2', test.readyState)
-// console.log('ipsolution3', test.getResponseHeader)
-// console.log('ipsolution3', test.getAllResponseHeaders)
-
-
-// tst git
 
 // Local Storage Creation
 
@@ -154,3 +191,33 @@ class uaInfo extends RequestInfo{
 $.ua.set(navigator.userAgent)
 console.log('test', $.ua.os);   
 console.log(navigator.userAgent)
+
+
+
+// Time TransfixedDate:
+// 1938
+
+
+const chi1 = document.getElementById('chiArt_1')
+const chi1Text = document.getElementById('chiArt_1_text')
+
+// Artist:
+// René Magritte
+// Belgian, 1898–1967
+fetch('https://api.artic.edu/api/v1/artworks/34181?fields=id,title,image_id')
+    .then(res=>res.json())
+    .then(json=>{
+        let this_url = `${json.config.iiif_url}/${json.data.image_id}/full/843,/0/default.jpg`
+        chi1.setAttribute('src', this_url)
+        chi1Text.textContent = `${json.data.title} - René Magritte (1938)`
+        })
+
+const chi2 = document.getElementById('chiArt_2')
+const chi2Text = document.getElementById('chiArt_2_text')
+fetch('https://api.artic.edu/api/v1/artworks/79600?fields=id,title,image_id')
+    .then(res=>res.json())
+    .then(json=>{
+        let this_url = `${json.config.iiif_url}/${json.data.image_id}/full/843,/0/default.jpg`
+        chi2.setAttribute('src', this_url)
+        chi2Text.textContent = `${json.data.title} - Fernand Léger (1919)`
+    })
